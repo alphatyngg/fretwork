6f2b879415ce536f58835d9acea8dca30b9aa2fb
alphatyngg <alphatyng@gmail.com>
2026-07-07 01:42:39 -0400

    Split into separate HTML, CSS, JS files

---
+++ script.js
@@ -0,0 +1,23 @@
+const NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+const SCALES = [
+    { id: "major",            label: "Major",            intervals: [0, 2, 4, 5, 7, 9, 11]},
+    { id: "minor",            label: "Minor",            intervals: [0, 2, 3, 5, 7, 8, 10]},
+    { id: "major_pentatonic", label: "Major Pentatonic", intervals: [0, 2, 4, 7, 9]},
+    { id: "minor_pentatonic", label: "Minor Pentatonic", intervals: [0, 3, 5, 7, 10]},
+    { id: "blues",            label: "Blues",            intervals: [0, 3, 5, 6, 7, 10]},
+    { id: "dorian",           label: "Dorian",           intervals: [0, 2, 3, 5, 7, 9, 10]},
+    { id: "mixolydian",       label: "Mixolydian",       intervals: [0, 2, 4, 5, 7, 9, 10]}
+];
+
+const STRINGS = ["e", "B", "G", "D", "A", "E"];          // high to low, standard tuning
+
+const state = {
+    root: 0,                // index into NOTE_NAMES - 0 = C
+    scaleId: "major",
+    labelMode: "name"       // "name" | "degree" | "interval"
+};
+
+console.log(NOTE_NAMES);
+console.log(SCALES);
+console.log(state);
