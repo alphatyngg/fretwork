5912c22426366f2e80fedf5f9418656e3eff94b6
alphatyngg <alphatyng@gmail.com>
2026-07-24 18:20:15 -0400

    Add chord wheel data model

--- circle.js
+++ circle.js
@@ -1,1 +1,17 @@
-console.log("circle.js loaded");
+const WHEEL_KEYS = [
+    { major: "C", minor: "Am", dim: "Bº" },
+    { major: "G", minor: "Em", dim: "F#º" },
+    { major: "D", minor: "Bm", dim: "C#º" },
+    { major: "A", minor: "F#m", dim: "G#º" },
+    { major: "E", minor: "C#m", dim: "D#º" },
+    { major: "B", minor: "G#m", dim: "A#º" },
+    { major: "F#", minor: "D#m", dim: "E#º" },
+    { major: "Db", minor: "Bbm", dim: "Cº" },
+    { major: "Ab", minor: "Fm", dim: "Gº" },
+    { major: "Eb", minor: "Cm", dim: "Dº" },
+    { major: "Bb", minor: "Gm", dim: "Aº" },
+    { major: "F", minor: "Dm", dim: "Eº" }
+];
+
+console.log(WHEEL_KEYS);
+console.log(WHEEL_KEYS.length);
